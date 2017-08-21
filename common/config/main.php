bd768ad8e03e999c3f169f346321824726300fc0
liufee <job@feehi.com>
2017-08-21 15:55:42 +0800

    udpate version to 0.1.3

--- common/config/main.php
+++ common/config/main.php
@@ -1,7 +1,7 @@
 <?php
 $config = [
     'name' => 'Feehi CMS',
-    'version' => '0.1.2',
+    'version' => '0.1.3',
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
