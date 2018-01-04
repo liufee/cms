990038c122de0fd64255c290a767190e82e575e6
liufee <job@feehi.com>
2018-01-04 20:48:16 +0800

    remove web/robots.txt from git repository

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -9,7 +9,7 @@ $params = array_merge(
 return [
     'id' => 'app-frontend',
     'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log','db'],
+    'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
     'components' => [
         'user' => [
