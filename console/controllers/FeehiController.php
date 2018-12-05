5123327bc311564c61e2cfe3a55cd14f10d365b9
unknown <job@feehi.com>
2018-12-05 12:47:46 +0800

    update install sql, update console feehi/download-upload-file

--- console/controllers/FeehiController.php
+++ console/controllers/FeehiController.php
@@ -8,6 +8,7 @@
 
 namespace console\controllers;
 
+use console\helpers\FileHelper;
 use Yii;
 use backend\models\form\Rbac;
 use console\controllers\helpers\BackendAuth;
@@ -21,27 +22,35 @@ class FeehiController extends Controller
     public function actionDownloadUploadFiles()
     {
         ini_set('memory_limit','1024M');
-        $this->stdout("获取uploads.zip" . PHP_EOL);
-        $this->stdout("环境准备中..." . PHP_EOL);
+        ini_set('default_socket_timeout', 1);
+        $uploadsZipUrl = "http://resource-1251086492.cossh.myqcloud.com/cms/uploads.zip";
+        $zipPhpUrl = "http://resource-1251086492.cossh.myqcloud.com/cms/pclzip.lib.php";
         $runtime = Yii::getAlias("@frontend/runtime/");
-        $zipphp = file_get_contents("http://resource-1251086492.cossh.myqcloud.com/cms/pclzip.lib.php");
-        file_put_contents($runtime . 'zip.php', $zipphp);
-        if( md5_file($runtime . 'zip.php') != "2334984a0c7c8bd0a05e3cea80b60aae" ){
-            $this->stdout("你的网络环境不安全，请通过手动方式下载uploads.zip" . PHP_EOL);
-            @unlink($runtime . 'zip.php');
-        }
-        require_once $runtime . 'zip.php';
         $this->stdout("正在下载uploads.zip请稍后..." . PHP_EOL);
         $uploadsZip = $runtime . "uploads.zip";
-        file_put_contents($uploadsZip, file_get_contents("http://resource-1251086492.cossh.myqcloud.com/cms/uploads.zip"));
-        $archive = new \PclZip($uploadsZip);
-        if ($archive->extract( PCLZIP_OPT_PATH, Yii::getAlias("@frontend/web/uploads"), PCLZIP_OPT_REMOVE_PATH, 'install/release') == 0 ) {
-            $this->stdout("获取配图失败，请手动下载uplaods.zip并解压到frontend/web/uploads目录" . PHP_EOL);
-        }else{
-            $this->stdout("获取配图成功" . PHP_EOL);
+        file_put_contents($uploadsZip, file_get_contents($uploadsZipUrl));
+        $this->stdout("下载uploads.zip完成" . PHP_EOL);
+        if( extension_loaded("zip") ){
+            FileHelper::unzip($uploadsZip, Yii::getAlias("@frontend/web/uploads/"));
+        }else {
+            $this->stdout("准备解zip包环境..." . PHP_EOL);
+            $zipphp = file_get_contents($zipPhpUrl);
+            file_put_contents($runtime . 'zip.php', $zipphp);
+            if (md5_file($runtime . 'zip.php') != "2334984a0c7c8bd0a05e3cea80b60aae") {
+                $this->stdout("你的网络环境不安全，请通过手动方式下载uploads.zip" . PHP_EOL);
+                @unlink($runtime . 'zip.php');
+                return 1;
+            }
+            require_once $runtime . 'zip.php';
+
+            $archive = new \PclZip($uploadsZip);
+            if ($archive->extract(PCLZIP_OPT_PATH, Yii::getAlias("@frontend/web/uploads"), PCLZIP_OPT_REMOVE_PATH, 'install/release') == 0) {
+                $this->stdout("获取配图失败，请手动下载uplaods.zip并解压到frontend/web/uploads目录" . PHP_EOL);
+            }
+            @unlink($runtime . 'zip.php');
         }
-        @unlink($runtime . 'zip.php');
         @unlink($uploadsZip);
+        $this->stdout("获取配图成功" . PHP_EOL);
     }
 
     public function actionPermission()
---
+++ console/helpers/FileHelper.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Author: lf
+ * Blog: https://blog.feehi.com
+ * Email: job@feehi.com
+ * Created at: 2018-12-05 12:23
+ */
+
+namespace console\helpers;
+
+class FileHelper extends \yii\helpers\FileHelper
+{
+    public static function unzip($zipFile, $path)
+    {
+        if (!file_exists($zipFile)) {
+            throw new Exception("File not exists " . $zipFile);
+        }
+        $resource = zip_open($zipFile);
+
+        while ($dirResource = zip_read($resource)) {
+            if (zip_entry_open($resource, $dirResource)) {
+                $file_name = $path . zip_entry_name($dirResource);
+                $file_path = substr($file_name, 0, strrpos($file_name, "/"));
+                if (!is_dir($file_path)) {
+                    mkdir($file_path, 0777, true);
+                }
+
+                if (!is_dir($file_name)) {
+                    $fileSize = zip_entry_filesize($dirResource);
+                    $file_content = zip_entry_read($dirResource, $fileSize);
+                    file_put_contents($file_name, $file_content);
+                }
+                zip_entry_close($dirResource);
+            }
+        }
+        zip_close($resource);
+    }
+}
