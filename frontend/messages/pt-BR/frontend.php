26dabaf53dc13c7af11c91f8b90daf6f88a54e2d
Gustavo G. Andrade <gugoan@users.noreply.github.com>
2019-10-13 20:38:41 -0300

    Frontend in pt-BR language
    Brazilian portuguese

---
+++ frontend/messages/pt-BR/frontend.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Author: lf
+ * Blog: https://blog.feehi.com
+ * Email: job@feehi.com
+ * Created at: 2016-04-18 09:41
+ */
+
+return [
+    /*public*/
+    'Hi, Log in' => 'Olá, faça login',
+    'Log out' => 'Sair',
+    'Sign up' => 'Cadastrar',
+    'About us' => 'Sobre nós',
+    'Contact us' => 'Contato',
+    'Search' => 'Pesquisar',
+    'Baidu' => 'Baidu',
+    'Please input keywords' => 'Digite palavras-chave',
+    'Effective,Professional,Conform to SEO' => 'Profissional, eficaz, em conformidade com SEO',
+    'Copyright, all rights reserved' => 'Copyright, todos os direitos reservados',
+    'If you forgot your password you can' => 'Se você esqueceu sua senha, pode',
+    'reset it' => 'resetá-la',
+    'Remember Me' => 'Lembrar-me',
+    'Well-choosen' => 'Bem escolhido',
+    'Latest Articles' => 'Últimos artigos',
+    'Related Recommends' => 'Recomendações relacionadas',
+    'Reproduced please indicate the source' => 'Reproduzido, indique a fonte',
+    'Post my comment' => 'Postar meu comentário',
+    'Change account' => 'Alterar conta',
+    'Cancel comment' => 'Cancelar comentário',
+    'Writing some...' => 'Escreva algo...',
+    'Submit comment' => 'Enviar comentário',
+    'emoj' => 'emoj',
+    'Hi, Please fill' => 'Oi, por favor preencha ',
+    'person posted' => 'pessoa postada',
+    'required' => 'Obrigatório',
+    'Website' => 'Website',
+    'The above error occurred while the Web server was processing your request.' => 'O erro acima ocorreu enquanto o servidor da Web estava processando sua solicitação',
+    'Please contact us if you think this is a server error. Thank you.' => 'Entre em contato conosco se achar que é um erro do servidor. Obrigado!',
+    'Guest' => 'Convidado',
+    /*public*/
+
+    /*site*/
+    'Hot Recommends' => 'Recomendações Quentes',
+    'Clound Tags' => 'Tags',
+    'Email Suscribe' => 'E-mail cadastrado',
+    'Friendly Links' => 'Links amigáveis',
+    'Latest Comments' => 'Últimos comentários',
+    'Subscribe to the wonderful content' => 'Assine o conteúdo maravilhoso',
+    ' Topics' => 'Tópicos',
+    'Subscribe' => 'Subscrever',
+    ' said' => ' disse',
+    ' Comments' => 'Comentários',
+    'This username has already been taken' => 'Este nome de usuário já foi usado',
+    'This email address has already been taken' => 'Este endereço de email já foi usado',
+    'Please fill out the following fields to signup' => 'Por favor, preencha os seguintes campos para inscrição',
+    'Signup' => 'Inscrição',
+    "None category named {name}" => 'Nenhuma categoria chamada {name}',
+    "Login" => "Login",
+    "Incorrect username or password." => "Usuário ou senha incorretos",
+    /*site*/
+
+    /*article*/
+    "Return Home" => 'Voltar ao início',
+    "Article id {id} is not exists" => "O código do artigo {id} não existe",
+    "Comment" => "Comentário",
+    "Only commented user can visit this article" => "Somente usuários comentados podem visitar este artigo,可以点击下方评论后查看",
+    'Like' => 'Curtir',
+    'Share' => 'Compartilhar',
+    "Please input the password of article id {id} : {article}" => "Insira a senha do ID do artigo {id}: {article}",
+    "Password error" => 'Erro de senha',
+    'Go' => 'Ir',
+    'Only login user can visit this article' => 'Somente o usuário de login pode visitar este artigo',
+    'Search keyword {keyword} results' => 'Pesquisar palavra-chave {keyword} results',
+    'Tag {tag} related articles' => 'Tag {tag} relacionada ao artigo',
+    'Category {cat} articles' => 'Categoria {cat}',
+    'Follow Wechat' => 'Seguir',
+    'Join group' => 'Participar do grupo',
+    'Official QQ group - main' => 'Grupo principal QQ group ',
+    'FeehiCMS official QQ group number: {number}' => 'Número oficial do grupo QQ do FeehiCMS: {number}',
+    'New generation CMS FeehiCMS' => 'Nova geração CMS FeehiCMS',
+    'Highly recommend' => 'Altamente recomendável',
+    'FeehiCMS based on yii2, support php7, makes website more excellent...' => 'FeehiCMS baseado no yii2, suporta php7, torna o site mais excelente ...',
+    'Send email at someone replied' => 'Enviar email para alguém respondido',
+    /*article*/
+
+    /*comment*/
+    'This article is not allowed to comment' => 'Este artigo não tem permissão para comentar',
+    'Website closed comment' => 'Comentário fechado do site',
+    'Comment waiting for approved.' => 'Comentário aguardando aprovação.',
+    'a minutes ago' => ' minutos atrás',
+    /*comment*/
+];
