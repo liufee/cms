f0cf3cae9dd31b6eb5934ff61986c431b9d37585
liufee <job@feehi.com>
2020-08-02 00:13:31 +0800

    fix:online install support sqlite

--- install/controllers/SiteController.php
+++ install/controllers/SiteController.php
@@ -360,14 +360,7 @@ class SiteController extends \yii\web\Controller
 
     private function checkAccountPermission(Connection $db, $dbtype, $dbname)
     {
-        if($dbtype == "sqlite") {
-            clearstatcache();
-            $path = str_replace("sqlite:", "", $db->dsn);
-            if (!is_really_writable($path)) {
-                throw new Exception($path . " is not writable");
-            }
-            return;
-        }else {
+        if($dbtype != "sqlite") {
             try {
                 $db->createCommand("use $dbname")->execute();
             }catch (\yii\db\Exception $e){
@@ -398,41 +391,5 @@ class SiteController extends \yii\web\Controller
             }
         }
 
-    }
-}
-
-if ( ! function_exists('is_really_writable'))
-{
-    function is_really_writable($file)
-    {
-        // If we're on a Unix server with safe_mode off we call is_writable
-        if (DIRECTORY_SEPARATOR == '/' AND @ini_get("safe_mode") == FALSE)
-        {
-            return is_writable($file);
-        }
-
-        // For windows servers and safe_mode "on" installations we'll actually
-        // write a file then read it.  Bah...
-        if (is_dir($file))
-        {
-            $file = rtrim($file, '/').'/'.md5(mt_rand(1,100).mt_rand(1,100));
-
-            if (($fp = @fopen($file, FOPEN_WRITE_CREATE)) === FALSE)
-            {
-                return FALSE;
-            }
-
-            fclose($fp);
-            @chmod($file, DIR_WRITE_MODE);
-            @unlink($file);
-            return TRUE;
-        }
-        elseif ( ! is_file($file) OR ($fp = @fopen($file, FOPEN_WRITE_CREATE)) === FALSE)
-        {
-            return FALSE;
-        }
-
-        fclose($fp);
-        return TRUE;
     }
 }
